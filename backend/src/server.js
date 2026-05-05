4563d0485d113cb61a514e46df0d67350ebe2632
PrachiMalviya06 <prachimalviya980@gmail.com>
2026-05-05 23:55:37 +0530

    fix backend deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
@@ -13,38 +12,48 @@ import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-const __dirname = path.resolve();
-
-// core middleware setup
+// =====================
+// MIDDLEWARE
+// =====================
 app.use(express.json());
-// credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(clerkMiddleware());
+
+// =====================
+// ROUTES
+// =====================
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "InterviewSync backend running successfully" });
+// health route (important for testing)
+app.get("/", (req, res) => {
+  res.send("🚀 InterviewSync Backend is Running");
 });
 
-// make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "Backend working fine ✅" });
+});
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
-// initialize backend server
+// =====================
+// SERVER START
+// =====================
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => console.log("InterviewSync server started on port:", ENV.PORT));
+
+    app.listen(ENV.PORT, () => {
+      console.log("🚀 Server started on port:", ENV.PORT);
+    });
   } catch (error) {
-    console.error("💥 Error starting the server", error);
+    console.error("💥 Error starting server:", error);
   }
 };
 
